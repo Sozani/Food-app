6df588036cf7ff9c1466df28a1be38eb4184a9cc
sozani <sys612603@gmail.com>
2023-01-15 00:08:11 +0200

    new initial commit

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -5,8 +5,8 @@ const MealsSummary = () => {
     <section className={classes.summary}>
       <h2>Delicious Food ,Delivered To You</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+        Choose your delicious favorite meal from our broad selection of
+        available meals and enjoy a delicious lunch or dinner at home.
       </p>
       <p>
         All our meals are cooked with high-quality ingredients, just-in-time and
