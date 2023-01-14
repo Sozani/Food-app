51dee6317e69291946b70862a8eb1df42169d40e
sozani <sys612603@gmail.com>
2023-01-14 22:55:01 +0200

    new project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
