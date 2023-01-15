62db485a2e6cc8895421825297dc09784940622a
sozani <sys612603@gmail.com>
2023-01-16 01:55:15 +0200

    Add cartItems

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,15 @@
 import classes from "./Cart .module.css";
 const Cart = (props) => {
-  return;
+  const cartItems = [{ id: "c1", name: "sushi", price: 12.99, amount: 2 }].map(
+    (item) => <li>item.name</li>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 export default Cart;
