31ba2776ef567c82c719acb64541a24074be2545
sozani <sys612603@gmail.com>
2023-01-16 03:26:52 +0200

    protals

--- src/components/UI/Module.js
+++ src/components/UI/Module.js
@@ -1,3 +1,26 @@
 import classes from "./Module.module.css";
-const Module = (props) => {};
+import { Fragment } from "react";
+import { ReactDOM } from "react";
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+const ModuleOverlay = (props) => {
+  return (
+    <div className={classes.module}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+const protalElement = document.getElementById("overlays");
+const Module = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, protalElement)}
+      {ReactDOM.createPortal(
+        <ModuleOverlay>{props.children}</ModuleOverlay>,
+        protalElement
+      )}
+    </Fragment>
+  );
+};
 export default Module;
