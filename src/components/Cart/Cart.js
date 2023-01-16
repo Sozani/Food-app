ffdeb6e779ea293cb3461a0223b3122f9706e783
sozani <sys612603@gmail.com>
2023-01-16 04:28:45 +0200

    changes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,10 @@
 import Modal from "../UI/Modal";
-import classes from "./Cart .module.css";
+import classes from "./Cart.module.css";
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "sushi", price: 12.99, amount: 2 }].map((item) => (
-        <li>item.name</li>
+      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
       ))}
     </ul>
   );
