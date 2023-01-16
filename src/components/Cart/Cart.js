0302366b28bf5578d0145d4af4d145c871cb2eff
sozani <sys612603@gmail.com>
2023-01-16 02:18:19 +0200

    div classes action with two button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -15,7 +15,10 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div></div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
     </div>
   );
 };
