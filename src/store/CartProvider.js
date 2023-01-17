ba0969073806c303e05627c2a2b831caa5c40367
sozani <sys612603@gmail.com>
2023-01-17 23:12:57 +0200

    add cartContext.provider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,2 +1,5 @@
-const CartProvider = (props) => {};
+import CartContext from "./Cart-context";
+const CartProvider = (props) => {
+  return <CartContext.Provider>{props.children}</CartContext.Provider>;
+};
 export default CartProvider;
