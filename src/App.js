e9c5c5fb861bbc02de55f20544b6017c77f6438f
sozani <sys612603@gmail.com>
2023-01-17 21:21:25 +0200

    use state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
+      {cartIsShown && <Cart />}
       <Header />
       <main>
         <Meals />
