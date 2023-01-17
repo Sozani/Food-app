2b16870a7e3728668618f3505a1d8e17fc3a9906
sozani <sys612603@gmail.com>
2023-01-17 23:53:19 +0200

    number of cart item

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -4,13 +4,16 @@ import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/Cart-context";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
