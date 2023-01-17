6073a3c882e34cb29998b7ae662165ce1fc22748
sozani <sys612603@gmail.com>
2023-01-17 21:24:04 +0200

    call showCartHandler

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <Fragment>
       {cartIsShown && <Cart />}
-      <Header />
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
